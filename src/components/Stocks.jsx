e23b2478eb7fdac9382d3cd8ffeb7bf2912a7e3b
INCTURE0\subiksha.venkatesh <subiksha.venkatesh@incture.com>
2022-10-22 01:16:02 +0530

    added wrapper for service call

--- src/components/Stocks.jsx
+++ src/components/Stocks.jsx
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
-
+import { fetchWrapper } from "../services/fetchWrapper";
 
 const Stocks = (props) => {
+
+    useEffect(() => {
+        getStocks();
+    }, [])
+    const getStocks = () => {
+        fetchWrapper("/InputService/v2/instruments",{method:"GET"})
+        .then((data) => {
+            console.log(data)
+        })
+    }
     return(<div>Stocks</div>)
 }
 
---
+++ src/services/fetchWrapper.js
@@ -0,0 +1,25 @@
+export const fetchWrapper = async (baseurl, options) => {
+  let splitUrl = baseurl.split("/");
+  let url = baseurl.replace(
+    "/" + splitUrl[1],
+    process.env["REACT_APP_" + splitUrl[1]]
+  );
+  const updateOptions = { ...options };
+  let result = [];
+
+  await fetch(url, updateOptions)
+    .then((response) => {
+      if (response.status >= 400 && response.status < 600) {
+        throw new Error("Bad response from server");
+      }
+      return response;
+    })
+    .then((returnedResponse) => returnedResponse.json())
+    .then((data) => {
+      result = data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return result;
+};
