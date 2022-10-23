6f66b91c1b5ee7d9466f7e74649ec19effd1a1c7
INCTURE0\subiksha.venkatesh <subiksha.venkatesh@incture.com>
2022-10-23 17:21:03 +0530

    creation table using stacks

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,7 +1,9 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Typography,IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchWrapper } from "../services/fetchWrapper";
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 
 const Quotes = (props) => {
     const { symbol } = useParams();
@@ -18,7 +20,9 @@ const Quotes = (props) => {
     useEffect(() => {
         getQuotes();
     }, [])
-
+    const sortAscending = () => {
+        
+    }
     return (<div>
         <Stack>
             <Stack direction="row"
@@ -34,23 +38,66 @@ const Quotes = (props) => {
                     padding: "15px",
                     marginTop: "20px"
                 }}>
-                {listHeader.map((item) => (
-                    <Typography>
-                        {item}
-                    </Typography>
-                ))}
+                {listHeader.map((item) => {
+                    if (item === "Time")
+                        return (
+                            <Stack direction="row">
+                                < Typography >
+                                    {item}
+                                </Typography>
+                                <IconButton
+                                    onClick={() => sortAscending()}
+                                    disableRipple
+                                    sx={{
+                                        ':hover': {
+                                            bgcolor: 'transparent',
+                                        },
+                                        '&.MuiIconButton-root':{
+                                            padding:0
+                                        }
+                                    }}
+                                >
+                                    <ArrowDownwardIcon />
+                                </IconButton>
+                                <IconButton
+                                    onClick={() => sortDescending()}
+                                    disableRipple
+                                    sx={{
+                                        ':hover': {
+                                            bgcolor: 'transparent',
+                                        },
+                                        '&.MuiIconButton-root':{
+                                            padding:0
+                                        }
+                                    }}
+                                >
+                                    <ArrowUpwardIcon />
+                                </IconButton>
+                            </Stack>)
+                    else 
+                        return (
+                            < Typography >
+                                {item}
+                            </Typography>
+                        )
+                })}
             </Stack>
-            <Stack>
+            <div style={{
+                height: "200px",
+                overflowY: "overlay",
+                width: "calc(100% - 45vh)",
+                alignItems: "center",
+                justifyContent: "flex-start",
+                marginLeft: "5vh",
+                boxShadow: "0px 1px 4px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.02), 0px 1px 12px rgba(0, 0, 0, 0.12)"
+            }}>
                 {allQuotes.map((row) => (
                     <Stack direction="row"
                         justifyContent="space-between"
                         alignItems="flex-start"
                         spacing={2}
                         sx={{
-                            width: "calc(100% - 50vh)",
-                            height: "25px",
                             background: '#FFFAFC',
-                            marginLeft: "5vh",
                             padding: "15px"
                         }}>
                         <Typography>
@@ -64,9 +111,9 @@ const Quotes = (props) => {
                         </Typography>
                     </Stack>
                 ))}
-            </Stack>
+            </div>
         </Stack>
-    </div>)
+    </div >)
 }
 
 export default Quotes;
